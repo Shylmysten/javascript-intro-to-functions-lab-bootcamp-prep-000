29ded00c46cc391bb2549f7b117f3bbae5c224d2
Tony Boswell <tboswell@kc.rr.com>
2017-11-09 04:02:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ var mixedCase = 'Hi there!'
 function sayHiToGrandma(string) {
   if (lowercase.toLowerCase() === lowercase) {
     return `I can't hear you!`;
-  }  {
+  } else if (uppercase.toUpperCase() === uppercase) {
+    return `YES INDEED!`;
+  } else if (string === `I love you, Grandma.`) {
+    return `I love you too.`;
+  }  else {
     console.log("FALSE!");
   }
 }
